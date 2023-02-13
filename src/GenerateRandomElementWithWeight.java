f9da598e0e4878bd6cac0378a064481c27f65355
Phaser2028 <dima.2000600@gmail.com>
2023-02-13 20:07:56 +0300

    v1

---
+++ src/GenerateRandomElementWithWeight.java
@@ -0,0 +1,30 @@
+public class GenerateRandomElementWithWeight {
+   private int[] valuesArr;
+   private int[] weightsArr;
+   public GenerateRandomElementWithWeight(int[] valuesArr, int[] weightsArr){
+      this.valuesArr = valuesArr;
+      this.weightsArr = weightsArr;
+   }
+   public int getRandValue(){
+      int sum_weight = 0;
+      int count = 0;
+      int n = 0;
+
+      for (int i: weightsArr) {
+         sum_weight += i;
+      }
+
+      int rand = (int) ((int)sum_weight* Math.random());
+
+
+      while (n < sum_weight){
+         count += weightsArr[n];
+         if(count >= rand ){
+            break;
+         }
+         n++;
+      }
+
+      return valuesArr[n];
+   }
+}
