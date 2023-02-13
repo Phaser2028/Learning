ebe01b18e336d19fd7888ddbc6d1b1268d013ffd
Phaser2028 <dima.2000600@gmail.com>
2023-02-14 02:21:09 +0300

    v0.1

---
+++ src/Node.java
@@ -0,0 +1,22 @@
+
+public class Node{
+    private int value;
+    private Node next;
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+}
+
