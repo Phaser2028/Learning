3470bf7768cbecf0504f897b5c2ad9dbb1083728
Phaser2028 <dima.2000600@gmail.com>
2023-02-16 23:01:08 +0300

    v1

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,12 +1,12 @@
 public class BubbleSort {
-    public BubbleSort(int[] array){
+    public BubbleSort(int[] array) {
 
-        for (int i = 0; i < array.length-1; i++) {
-            for (int j = 0; j < array.length-1; j++) {
-                if(array[j]>array[j+1]){
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
                     int a = array[j];
-                    array[j]=array[j+1];
-                    array[j+1]=a;
+                    array[j] = array[j + 1];
+                    array[j + 1] = a;
                 }
             }
         }
