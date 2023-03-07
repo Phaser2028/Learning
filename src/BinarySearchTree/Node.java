0de8ed5a3a491079c294a12b35bb522e5d493970
Phaser2028 <dima.2000600@gmail.com>
2023-03-07 09:03:23 +0300

    with recursion

--- src/BinarySearchTree/Node.java
+++ src/BinarySearchTree/Node.java
@@ -12,48 +12,49 @@ public class Node {
         this.value = value;
 
     }
-    Node node = this;
-    public void insert(int key, String data) {
-        Node node = this;
 
-        if (node.key==key) {
+    public static Node get(Node node, int key) {
+
+        if (node.right != null) {
+            if (key > node.key) {
+                node = node.right;
+                get(node, key);
+            }
+        }
+
+        if (node.left != null) {
+            if (key < node.key) {
+                node = node.left;
+                get(node, key);
+            }
+        } else {
+            return node;
+        }
+        return get(node, key);
+    }
+
+    public static void insert(Node node, int key, String data) {
+
+
+        if (node.key == key) {
             node.value = data;
-        } else if (key > node.key) {
+        }
+        if (key > node.key) {
             if (node.right != null) {
                 node = node.right;
-                get(key);
+                insert(node, key,data);
             } else {
                 node.right = new Node(key, data);
             }
         } else {
             if (node.left != null) {
                 node = node.left;
-                get(key);
+                insert(node, key,data);
             } else {
                 node.left = new Node(key, data);
             }
         }
     }
-    public Node get(int key) {
-
-        if (node.key==key) {
-            return node;
-        }
-        if (key > node.key) {
-            node=node.right;
-            get(key);
-        }
-        if (key < node.key) {
-            node=node.right;
-            get(key);
-        }
-        return get(key);
-
-
-    }
-
-
-
     @Override
     public String toString() {
         return "Node{" +
