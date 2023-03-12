ef1d2dec9824b182a66fd8748646256d736625d4
Phaser2028 <dima.2000600@gmail.com>
2023-03-12 22:17:58 +0300

    commit

--- src/HackerRank/Solutions.java
+++ src/HackerRank/Solutions.java
@@ -1,5 +1,9 @@
 package HackerRank;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 public class Solutions {
 
     public static void Splitter(String str) {
@@ -16,7 +20,7 @@ public class Solutions {
         }
     }
 
-    static boolean isAnagram(String a, String b) {
+    public static boolean isAnagram(String a, String b) {
         char[] arr = a.concat(b).toLowerCase().toCharArray();
         int n = 0;
         for (char c : arr) {
@@ -32,7 +36,17 @@ public class Solutions {
         }
         return true;
     }
-    public static void PatternCompileValidator(String str){
 
+    public static void PatternCompileValidator(String str) {
+        Scanner in = new Scanner(System.in);
+
+        try {
+            Pattern.compile(str);
+            System.out.println("Valid");
+
+        } catch (PatternSyntaxException | NumberFormatException e) {
+            System.out.println("Invalid");
+
+        }
     }
 }
