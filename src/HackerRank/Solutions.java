948ef0bd68e709c795ef1a813c6d2c708d17a065
Phaser2028 <dima.2000600@gmail.com>
2023-03-12 19:39:15 +0300

    commit

---
+++ src/HackerRank/Solutions.java
@@ -0,0 +1,38 @@
+package HackerRank;
+
+public class Solutions {
+
+    public static void Splitter(String str) {
+        str = str.trim();
+        String[] result = str.split("[ ,!?._'@]+");
+
+        if (str.isEmpty()) {
+            System.out.println(0);
+        } else {
+            System.out.println(result.length);
+        }
+        for (String v : result) {
+            System.out.println(v);
+        }
+    }
+
+    static boolean isAnagram(String a, String b) {
+        char[] arr = a.concat(b).toLowerCase().toCharArray();
+        int n = 0;
+        for (char c : arr) {
+            for (char value : arr) {
+                if (c == value) {
+                    n++;
+                }
+            }
+            if (n % 2 != 0) {
+                return false;
+            }
+            n = 0;
+        }
+        return true;
+    }
+    public static void PatternCompileValidator(String str){
+
+    }
+}
