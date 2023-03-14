41e9f95ae7490aec26a76416ddc566bb1d38bc02
Phaser2028 <dima.2000600@gmail.com>
2023-03-14 21:55:09 +0300

    commit

--- src/HackerRank/Solutions.java
+++ src/HackerRank/Solutions.java
@@ -1,11 +1,42 @@
 package HackerRank;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 public class Solutions {
+
+    public void Lister(){
+        Scanner in = new Scanner(System.in);
+        String[] arr;
+        ArrayList<Integer> list = new ArrayList<>();
+        //in.nextLine().replaceAll("\\s+", "").split("")
+
+        while (true) {
+
+            int len = in.nextInt();
+
+           arr = in.nextLine().replaceAll("\\s+", "").split("");
+           list = new ArrayList<>(Stream.of(Arrays.asList(arr)).forEach(x->Integer.parseInt(x.toString())));
+
+
+
+
+
+
+        }
+    }
+
+
+
 
     public static void Splitter(String str) {
         str = str.trim();
