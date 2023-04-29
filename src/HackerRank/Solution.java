66e91539acf6fec32fa9674515eeab6d56ead7c3
Phaser2028 <dima.2000600@gmail.com>
2023-04-29 17:32:17 +0300

    add uniqueNumbersInSubDequeCounter method

--- src/HackerRank/Solution.java
+++ src/HackerRank/Solution.java
@@ -13,9 +13,38 @@ import java.util.stream.Stream;
 public class Solution {
 
 
-        //https://www.hackerrank.com/challenges/java-dequeue
+    //https://www.hackerrank.com/challenges/java-dequeue
+    public void uniqueNumbersInSubDequeCounter() {
+        Scanner in = new Scanner(System.in);
+        Deque<Integer> deque = new ArrayDeque<>();
+        Set<Integer> set = new HashSet<>();
+
+        int n = in.nextInt();
+        int m = in.nextInt();
 
 
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int num = in.nextInt();
+
+            deque.add(num);
+            set.add(num);
+
+            if (deque.size() == m) {
+                if (set.size() > max) {
+                    max = set.size();
+                }
+
+                int first = deque.remove();
+                if (!deque.contains(first)) {
+                    set.remove(first);
+                }
+
+            }
+        }
+        System.out.println(max);
+    }
 
 
     //https://www.hackerrank.com/challenges/java-comparator
