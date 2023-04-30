c0fdce0acfeb40326a6a1d3b0681d1a2e2de4a98
Phaser2028 <dima.2000600@gmail.com>
2023-04-30 21:41:15 +0300

    add isPalindrome

--- src/LeetCode/LeetCode.java
+++ src/LeetCode/LeetCode.java
@@ -2,6 +2,20 @@ package LeetCode;
 
 public class LeetCode {
 
+
+    //https://leetcode.com/problems/palindrome-number
+    public static boolean isPalindrome(int x){
+        String nums = String.valueOf(x);
+
+        for (int i = 0; i < nums.length()/2; i++) {
+            if(nums.charAt(i) !=nums.charAt(nums.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
     //https://leetcode.com/problems/two-sum
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
