f1cbb21701a690abd9c4140c4503e4cd1098da84
Phaser2028 <dima.2000600@gmail.com>
2023-05-03 04:00:44 +0300

    add romanToInt

--- src/LeetCode/LeetCode.java
+++ src/LeetCode/LeetCode.java
@@ -1,14 +1,48 @@
 package LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LeetCode {
+
+    //https://leetcode.com/problems/roman-to-integer
+    public static int romanToInt(String s) {
+
+        Map<Character, Integer> dictionary = new HashMap<>();
+        dictionary.put('I', 1);
+        dictionary.put('V', 5);
+        dictionary.put('X', 10);
+        dictionary.put('L', 50);
+        dictionary.put('C', 100);
+        dictionary.put('D', 500);
+        dictionary.put('M', 1000);
+
+        int res = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if(i+1==s.length()){
+                res += dictionary.get(s.charAt(i));
+                return res;
+            }
+
+            if (dictionary.get(s.charAt(i)) < dictionary.get(s.charAt(i+1))) {
+                res += (dictionary.get(s.charAt(i+1)) - dictionary.get(s.charAt(i)));
+                i++;
+            } else {
+                res += dictionary.get(s.charAt(i));
+            }
+        }
+        return res;
+    }
 
 
     //https://leetcode.com/problems/palindrome-number
-    public static boolean isPalindrome(int x){
+    public static boolean isPalindrome(int x) {
         String nums = String.valueOf(x);
 
-        for (int i = 0; i < nums.length()/2; i++) {
-            if(nums.charAt(i) !=nums.charAt(nums.length()-i-1)){
+        for (int i = 0; i < nums.length() / 2; i++) {
+            if (nums.charAt(i) != nums.charAt(nums.length() - i - 1)) {
                 return false;
             }
         }
@@ -19,9 +53,9 @@ public class LeetCode {
     //https://leetcode.com/problems/two-sum
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if(nums[i]+nums[j]==target)
-                    return new int[]{i,j};
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[]{i, j};
             }
         }
 
