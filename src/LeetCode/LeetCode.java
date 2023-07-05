5da7ad3c2238074277979324ba1bf8f9a90a58fe
Phaser2028 <dima.2000600@gmail.com>
2023-07-05 20:32:43 +0300

    add binary-search solution

--- src/LeetCode/LeetCode.java
+++ src/LeetCode/LeetCode.java
@@ -4,7 +4,24 @@ import java.util.*;
 
 public class LeetCode {
 
+    //https://leetcode.com/problems/binary-search
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid;
 
+        while (low <= high) {
+            mid = low + ((high - low) / 2);
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else if (nums[mid] > target) {
+                high = mid - 1;
+            } else if (nums[mid] == target) {
+                return mid;
+            }
+        }
+        return -1;
+    }
 
     //https://leetcode.com/problems/product-of-array-except-self
     public static int[] productExceptSelf(int[] nums) {
@@ -61,7 +78,6 @@ public class LeetCode {
             map.remove(longestKey);
             i++;
         }
-
 
         return res;
     }
