b68b98e63e613411e7fce669352c4287bc1df877
Dmitriy <dima.2000600@gmail.com>
2023-09-18 16:52:15 +0300

    ADD: solution to problem 283

--- src/LeetCode/LeetCode.java
+++ src/LeetCode/LeetCode.java
@@ -4,6 +4,22 @@ import java.util.*;
 
 public class LeetCode {
 
+    //https://leetcode.com/problems/move-zeroes
+    public static void moveZeroes(int[] nums) {
+        int n = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]!=0){
+                nums[n] = nums[i];
+                n++;
+            }
+        }
+        while(n<nums.length){
+            nums[n] = 0;
+            n++;
+        }
+    }
+
     //https://leetcode.com/problems/two-sum
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> map = new HashMap<>();
