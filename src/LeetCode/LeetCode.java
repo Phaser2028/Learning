f3ef4148370af8a009b0c27a061c8d27905e63aa
Dmitriy <dima.2000600@gmail.com>
2023-09-18 17:18:45 +0300

    ADD: solution to problem 392

--- src/LeetCode/LeetCode.java
+++ src/LeetCode/LeetCode.java
@@ -4,6 +4,21 @@ import java.util.*;
 
 public class LeetCode {
 
+    //https://leetcode.com/problems/is-subsequence
+    public static boolean isSubsequence(String s, String t) {
+
+        int i = 0 , j = 0;
+
+        while(i < s.length() && j < t.length()){
+            if(s.charAt(i) == t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+
+
     //https://leetcode.com/problems/move-zeroes
     public static void moveZeroes(int[] nums) {
         int n = 0;
